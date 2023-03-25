f676254873af937272c08725d2d91ea3082d1a82
KyeongsooLee <leeksca26@gmail.com>
2023-03-25 19:58:02 -0400

    Send email with Gmail, Activation Email

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -1,6 +1,7 @@
 const Users = require('../models/userModel')
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
+const sendMail = require('./sendMail')
 
 const {CLIENT_URL} = process.env
 
@@ -30,15 +31,41 @@ const userCtrl = {
             const activation_token = createActivationToken(newUser)
 
             const url = `${CLIENT_URL}/user/activate/${activation_token}`
-            SendmailTransport(email, url)
-
+            sendMail(email, url)
             
+
             res.json({msg: "Register Success! Please activate your email to start."})
         } catch (err) {
             return res.status(500).json({msg: err.message})
         }
-    }
+    },
+    activateEmail: async(req, res) => {
+        try {
+            const {activation_token} = req.body
+            const user = jwt.verify(activation_token, process.env.ACTIVATION_TOKEN_SECRET)
+
+            const {name, email, password} = user
+
+            const check = await Users.findOne({email})
+            if(check) return res.status(400).json({msg: "This email already exists."})
+
+            const newUser = new Users({
+                name, email, password
+            })
+
+            await newUser.save()
+
+            res.json({msg: "Account has been activated!"})
+
+        } catch (err) {
+            return res.status(500).json({msg: err.message})
+        }
+    },
+    
 }
+
+
+
 
 
 function validateEmail(email) {
