5cd5077c4368f6f4987291ca0dcfa3bfa304cc5a
KyeongsooLee <leeksca26@gmail.com>
2023-03-26 23:08:34 -0400

    Middleware and Forgot, Reset Password

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -31,7 +31,7 @@ const userCtrl = {
             const activation_token = createActivationToken(newUser)
 
             const url = `${CLIENT_URL}/user/activate/${activation_token}`
-            sendMail(email, url)
+            sendMail(email, url, "Verify your email address")
             
 
             res.json({msg: "Register Success! Please activate your email to start."})
@@ -94,6 +94,37 @@ const userCtrl = {
                 res.json({access_token})
                 console.log(user)
             })
+        } catch (err) {
+            return res.status(500).json({msg: err.message})
+        }
+    },
+    forgotPassword: async (req, res) => {
+        try {
+            const {email} = req.body
+            const user = await Users.findOne({email})
+            if(!user) return res.status(400).json({msg: "This email does not exist."})
+
+            const access_token = createAccessToken({id: user._id})
+            const url = `${CLIENT_URL}/user/reset/${access_token}`
+
+            sendMail(email, url, "Reset your password")
+            res.json({msg: "Re-send the password, please check your email."})
+        } catch (err) {
+            return res.status(500).json({msg: err.message})
+        }
+    },
+    resetPassword: async (req, res) => {
+        try {
+            const {password} = req.body
+            console.log(password)
+            const passwordHash = await bcrypt.hash(password, 12)
+
+            console.log(req.user)
+            await Users.findOneAndUpdate({_id: req.user.id}, {
+                password: passwordHash
+            })
+
+            res.json({msg: "Password successfully changed!"})
         } catch (err) {
             return res.status(500).json({msg: err.message})
         }
